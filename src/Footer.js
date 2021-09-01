aa09452d5bdb832b78988d97dc751b04d68e52a4
a20l <aidaytyn@gmail.com>
2021-09-01 14:40:47 -0500

    added movie-trailer

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'
+            style={{
+                textAlign: 'center',
+                color: 'white',
+                paddingBottom: '10px'
+            }}
+        >
+            Aika 2021
+        </div>
+    )
+}
+
+
+
+export default Footer
--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import YouTube from 'react-youtube';
 import axios from './axios'
-import requests from './requests';
 import "./Row.css";
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     // a snipped of code which runs based on a specific condition/variable
 
@@ -23,20 +25,50 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }, [fetchUrl])
         // if [movies] it runs once and every time movies changes
 
+        const opts = {
+            height: "390",
+            width: '100%',
+            playerVars: {
+                autoplay: 1,
+            }
+        }
     
+
+    const handleClick = (movie) => {
+        if(trailerUrl){
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.title || "")
+            .then((url) => {
+              
+                //https://www.youtube.com/watch?v=0zvSZhTeFgM
+                const urlParams = new URLSearchParams( new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error))
+        }
+    }
+console.log(movies)
     return (
         <div className='row'>
             <h2>{title}</h2>
             <div className='row_posters'>
-                {/* container -> poster */}
-                {movies.map(movie => (
-                    <img 
-                        className={`row_poster ${isLargeRow && "row_posterLarge"}`} 
+               
+                {movies.map(
+                    (movie) => 
+                        (isLargeRow && movie.poster_path) || 
+                        (!isLargeRow && movie.backdrop_path && (
+                            <img 
                         key={movie.id}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                        onClick={() => handleClick(movie)}
+                        className={`row_poster ${isLargeRow && "row_posterLarge"}`} 
+                        src={`${base_url}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} />
-                ))}
+                        ))
+                    
+                )}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
