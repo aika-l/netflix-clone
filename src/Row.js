ec56f1fd43d55c6f187129325eb46779f5f14e26
a20l <aidaytyn@gmail.com>
2021-09-01 12:20:04 -0500

    added banner

--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     // a snipped of code which runs based on a specific condition/variable
@@ -14,7 +14,7 @@ function Row({ title, fetchUrl }) {
 
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            //console.log(request); to virify if it returned anything back
+            //console.log(request); //to virify if it returned anything back
             setMovies(request.data.results)
             return request;
         }
@@ -31,9 +31,9 @@ function Row({ title, fetchUrl }) {
                 {/* container -> poster */}
                 {movies.map(movie => (
                     <img 
-                        className='row_poster' 
+                        className={`row_poster ${isLargeRow && "row_posterLarge"}`} 
                         key={movie.id}
-                        src={`${base_url}${movie.poster_path}`} 
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} />
                 ))}
             </div>
